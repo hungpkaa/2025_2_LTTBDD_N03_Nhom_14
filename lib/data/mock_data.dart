e1cf4c633eaa4a89e372b6631edd5ad5985c21c9
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-11 23:42:27 +0700

    Add mock song data

--- lib/data/mock_data.dart
+++ lib/data/mock_data.dart
@@ -1,1 +1,47 @@
+import '../models/song.dart';
 
+List<Song> songs = [
+  Song(
+    title: "Chạy ngay đi ",
+    artist: "Sơn Tùng M-TP",
+    imageUrl: "assets/images/ab1.jpg",
+  ),
+
+  Song(title: "Bạc Phận", artist: "Jack 97", imageUrl: "assets/images/ab2.jpg"),
+
+  Song(
+    title: "Tháng năm ",
+    artist: "Soobin",
+    imageUrl: "assets/images/ab3.jpg",
+  ),
+
+  Song(
+    title: "Chạm khẽ tim anh một chút thôi",
+    artist: "Noo Phước Thịnh",
+    imageUrl: "assets/images/ab4.jpg",
+  ),
+
+  Song(
+    title: "Chạm khẽ tim anh một chút thôi",
+    artist: "Noo Phước Thịnh",
+    imageUrl: "assets/images/ab4.jpg",
+  ),
+
+  Song(
+    title: "Chạm khẽ tim anh một chút thôi",
+    artist: "Noo Phước Thịnh",
+    imageUrl: "assets/images/ab4.jpg",
+  ),
+
+  Song(
+    title: "Chạm khẽ tim anh một chút thôi",
+    artist: "Noo Phước Thịnh",
+    imageUrl: "assets/images/ab4.jpg",
+  ),
+
+  Song(
+    title: "Chạm khẽ tim anh một chút thôi",
+    artist: "Noo Phước Thịnh",
+    imageUrl: "assets/images/ab4.jpg",
+  ),
+];
