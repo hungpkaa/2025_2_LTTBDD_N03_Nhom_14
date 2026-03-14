8a2a986669f4ca6537379ccab5e74251d173079d
DsChien102 <22010008@st.phenikaa-uni.edu.vn>
2026-03-15 01:13:51 +0700

    them data ,chinh playlist

--- lib/data/mock_data.dart
+++ lib/data/mock_data.dart
@@ -24,24 +24,38 @@ List<Song> songs = [
   Song(
     title: "Chạm khẽ tim anh một chút thôi",
     artist: "Noo Phước Thịnh",
-    imageUrl: "assets/images/ab4.jpg",
+    imageUrl: "assets/images/ab5.jpg",
   ),
 
   Song(
     title: "Chạm khẽ tim anh một chút thôi",
     artist: "Noo Phước Thịnh",
-    imageUrl: "assets/images/ab4.jpg",
+    imageUrl: "assets/images/ab6.jpg",
   ),
 
   Song(
     title: "Chạm khẽ tim anh một chút thôi",
     artist: "Noo Phước Thịnh",
-    imageUrl: "assets/images/ab4.jpg",
+    imageUrl: "assets/images/ab7.jpg",
   ),
 
   Song(
     title: "Chạm khẽ tim anh một chút thôi",
     artist: "Noo Phước Thịnh",
-    imageUrl: "assets/images/ab4.jpg",
+    imageUrl: "assets/images/ab1.jpg",
   ),
+];
+
+class Playlist {
+  final String title;
+  final String image;
+
+  Playlist({required this.title, required this.image});
+}
+
+List<Playlist> playlists = [
+  Playlist(title: "Top Hits", image: "assets/images/ab8.png"),
+  Playlist(title: "Chill Vibes", image: "assets/images/ab9.jpg"),
+  Playlist(title: "Hit Hop", image: "assets/images/ab10.jpg"),
+  Playlist(title: "Daily Mix", image: "assets/images/ab11.jpg"),
 ];
